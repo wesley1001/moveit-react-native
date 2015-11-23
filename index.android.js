f0b7eccb608014a98121f1e0e37ec46d57fd2386
Peter Lazar <peterlazar1993@gmail.com>
2015-11-23 19:48:06 +0530

    +Muraleekrishna| static version of add entry page

--- index.android.js
+++ index.android.js
@@ -3,49 +3,78 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
+var request = require('superagent');
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
+  ToolbarAndroid,
+  TouchableHighlight,
+  TextInput,
+  Component,
   Text,
   View,
+  ProgressBarAndroid
 } = React;
 
-var ReMoveIt = React.createClass({
-  render: function() {
+class ReMoveIt extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: false};
+  }
+
+  fetchData() {
+    request
+  .post('http://staging-move1t.herokuapp.com/entries.json')
+  .send({ email: 'peter.l@multunus.com',
+        entry: {
+          duration: "30",
+          date: "2015-12-23 12:01:02",
+          description: 'test'
+        }
+      })
+  .set('Accept', 'application/json')
+  .end((err, res) => {
+    // Calling the end function will send the request
+    this.setState({isLoading: false});
+    console.log(res);
+  });
+}
+
+  onSubmit() {
+    this.setState({isLoading: true});
+    this.fetchData();
+  }
+
+  render() {
+    let spinner = this.state.isLoading ? (<ProgressBarAndroid styleAttr="Inverse"/>) : (<View/>);
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <View>
+        {spinner}
+        <ToolbarAndroid
+          style={styles.toolbar}
+        />
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          placeholder='Enter Email'
+        />
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        placeholder='Enter Minutes'
+      />
+    <TouchableHighlight
+      onPress={this.onSubmit.bind(this)}>
+      <Text>Submit</Text>
+    </TouchableHighlight>
+    </View>
     );
   }
-});
+}
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  toolbar: {
+    backgroundColor: '#fdc300',
+    height: 56,
   },
 });
 
